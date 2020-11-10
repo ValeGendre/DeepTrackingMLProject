5d71e781afd1424c36494706c1f107fda9de231e
MagOzon <magaliozon@gmail.com>
2020-11-10 17:11:42 +0100

    dataset.py update

--- resources/dataset.py
+++ resources/dataset.py
@@ -1,15 +1,27 @@
-import torch
+import torch, os,cv2
+import numpy as np
 from torch.utils.data import Dataset,DataLoader
 
 class MiceData(Dataset):
     def __init__(self):
-        pass
+        self.root_dir = root_dir
+        self.image_path = os.path.join(root_dir, 'all_frames') # Warning : do not rename all_frames folder
+        # How many video do we have ? How many frames per video do we have ?
+        idx = 1
+        res = 0
+        self.framepervidnb = []
+        while res==0:
+            vidinb = len([f for f in os.listdir(data.image_path) if f.startswith('vid{}'.format(idx)) and os.path.isfile(os.path.join(data.image_path, f))])
+            if vidinb == 0:
+                res=1
+            else :
+                self.framepervidnb.append(vidinb)
+                idx +=1
+        self.vidnb = idx-1
 
     def __len__(self):
-        pass
+        self.size = sum(self.framepervidnb) - self.vidnb
+        return self.size
 
     def __getitem__(self, i):
         pass
-
-
-# Coucou
