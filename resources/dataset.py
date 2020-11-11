3ce56a03ecd5a91fb49739225b7b36ce7e014a74
ValeGendre <vgendre.perso@gmail.com>
2020-11-11 17:07:21 +0100

    Fixed not being able to show img

--- resources/dataset.py
+++ resources/dataset.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset,DataLoader
 
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
 class MiceData(Dataset):
     def __init__(self,root_dir):
         # Enter your own root_dir (for instance : \resources\data)
