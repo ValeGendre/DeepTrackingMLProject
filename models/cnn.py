c0bace93b4abcb64f30c5423ba491a5d4ca3ff6a
MagOzon <magaliozon@gmail.com>
2020-11-21 17:35:26 +0100

    cnn.py complete

--- models/cnn.py
+++ models/cnn.py
@@ -4,4 +4,12 @@ from torch import nn
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-    
+            self.C1 = nn.Conv2d(1, 32, 3)            
+            self.C2 = nn.Conv2d(32, 32, 3)
+            self.Norm = nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=1)
+        
+
+    def forward(self, x):
+        x = F.relu(self.C1(x))
+        x = Norm(self.C2(x))
+        return x
