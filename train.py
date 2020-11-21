362c5ef25d739cdc82cb6a97c24272a0142280d1
MagOzon <magaliozon@gmail.com>
2020-11-21 20:55:51 +0100

    train.py data stuff

--- train.py
+++ train.py
@@ -1,1 +1,17 @@
+
+from resources.dataset import MiceData #jetentendspasvscode
+
 import torch
+from torch.utils.data import DataLoader
+
+# Parameters init
+batch_size = 32
+
+# Path to all_frames
+root_dir = ""
+# Number of consecutive frames
+n = 2
+
+# Loading the data
+miceset = MiceData(root_dir=root_dir,n=n)
+miceset_loader = DataLoader(miceset, batch_size=batch_size, shuffle=True, pin_memory=True, drop_last=True)
