7489c8664b2161dbf678c31217677f03fcd35005
MagOzon <magaliozon@gmail.com>
2020-12-04 11:02:06 +0100

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -1,6 +1,17 @@
+import torch.nn as F
+import numpy as np
+
 def loss(forward_labels, backward_labels):
-    pass
+    losss = F.MSELoss()
+    output = losss(backward_labels,forward_labels)
+    #output.backward() # put in the train loop
+    return output
 
 def rd_BB():
-    pass
+    # Random init 
+    x = np.random.choice(250)
+    y = np.random.choice(250)
+    dx = 30
+    dy = 30
+    return x, y, dx, dy
 
